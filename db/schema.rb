85280ebfdb549249abe43c16f4eac2fc77c2f0f0
kazuki <s.kazuki.japan@gmail.com>
2016-12-12 05:28:28 +0900

    userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204082935) do
+ActiveRecord::Schema.define(version: 20161211202533) do
 
   create_table "admin_blogs", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20161204082935) do
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
